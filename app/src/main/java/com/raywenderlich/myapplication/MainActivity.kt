42e0cebd2fae1ebd42870e9677bfab0f4e0550d1
HAIND <HAIND haind@hqsoft.com.vn>
2022-09-18 12:54:58 +0700

    Initial commit

---
+++ app/src/main/java/com/raywenderlich/myapplication/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.raywenderlich.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.raywenderlich.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
